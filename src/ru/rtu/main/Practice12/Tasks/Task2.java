86bd83f5525af91fe6036255c1b36ed110701006
K03bIPb <headshot2001@mail.ru>
2020-10-25 00:10:21 +0300

    Task 12 task 2 begins

--- src/ru/rtu/main/Practice12/Tasks/Task2.java
+++ src/ru/rtu/main/Practice12/Tasks/Task2.java
@@ -1,5 +1,32 @@
 package ru.rtu.main.Practice12.Tasks;
 
-public class Task2 {
+import java.util.Scanner;
+import java.util.TreeSet;
 
+public class Task2 {
+    public static void main(String[] args) {
+        TreeSet<String> treeSet = new TreeSet<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            String input = scanner.nextLine();
+            String[] split = input.split(" ");
+            if (split.length <= 2) {
+                if (split[0].equals("LIST")) {
+                    System.out.println(treeSet);
+                }
+                if (split[0].equals("ADD")) {
+                    if (split[1].contains("@")){
+                        if (split[1].contains(".")){
+                        String[] split2 = split[1].split(".");
+                        String address = "";
+                        for (int i = 1; i < split.length; i++)
+                            address = address + split[i];
+                        treeSet.add(address);
+                    }
+                }
+            }
+        }
+
+    }
 }
