e292e8e21ec344f51492399155b1d85a012ca810
K03bIPb <headshot2001@mail.ru>
2020-10-24 12:08:38 +0300

    Prac 11 task 1 fixed

--- src/ru/rtu/main/Practice11/task1.java
+++ src/ru/rtu/main/Practice11/task1.java
@@ -3,23 +3,13 @@ package ru.rtu.main.Practice11;
 public class task1 {
     public static void main(String[] args) {
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-        String[] res = new String[text.length()];
-        for (int i = 0; i < res.length; i++) {
-            int textIndex = text.indexOf(' ', text.indexOf(' ') + 1) + 1;
-            res[i] = text.substring(textIndex, ' ');
-            System.out.println(res[i]);
-        }
+        String[] res = new String[2];
 
-
-        int[] nums = new int[100];
-        int a = 0, b = 0, x = 0;
-        for (int i = 0; i < res.length; i++) {
-            try {
-                nums[x] = Integer.parseInt(res[i]);
-                x++;
-            } catch (NumberFormatException ignored) {}
-        }
-        int result = nums[0] + nums[2];
+        int textIndex = text.indexOf(' ', text.indexOf(' ') + 1) + 1;
+        res[0] = text.substring(textIndex, text.indexOf(' ', textIndex));
+        int textLastIndex = text.lastIndexOf(' ', text.lastIndexOf(' ')-1) + 1;
+        res[1] = text.substring(textLastIndex, text.indexOf(' ', textLastIndex));
+        int result = Integer.parseInt(res[0]) + Integer.parseInt(res[1]);
         System.out.println("Result: " + result);
     }
 }
